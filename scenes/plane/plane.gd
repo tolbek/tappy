2d593f15277e94c6ecd64f1836be1af9ec30a8cc
tolbek <ahmetfiratcs@gmail.com>
2025-03-02 19:19:31 +0300

    plane flight code

--- scenes/plane/plane.gd
+++ scenes/plane/plane.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const GRAVITY: float = 600.0
-
+const POWER: float = -200.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,4 +13,10 @@ func _physics_process(delta: float) -> void:
 
 	velocity.y += GRAVITY * delta
 
+	fly()
+
 	move_and_slide()
+	
+func fly() -> void:
+	if Input.is_action_just_pressed("fly") == true:
+		velocity.y = POWER
