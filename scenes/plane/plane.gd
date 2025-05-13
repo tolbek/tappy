5c29a3da87e947e798ce90c80e7bbab8b0a01917
tolbek <ahmetfiratcs@gmail.com>
2025-05-13 20:06:43 +0300

    when pressed space rotating animation
    30 degree animation update

--- scenes/plane/plane.gd
+++ scenes/plane/plane.gd
@@ -4,6 +4,7 @@ const GRAVITY: float = 1000.0
 const POWER: float = -350.0
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -25,6 +26,7 @@ func _physics_process(delta: float) -> void:
 func fly() -> void:
 	if Input.is_action_just_pressed("fly") == true:
 		velocity.y = POWER
+		animation_player.play("power")
 
 func die () -> void:
 	animated_sprite_2d.stop()
