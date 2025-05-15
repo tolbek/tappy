fdd8c8995baa27830c1ab3574297d67e783355d6
tolbek <ahmetfiratcs@gmail.com>
2025-05-15 19:06:49 +0300

    physics process formula

---
+++ Scenes/Plane/Plane.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+var _gravity: float = ProjectSettings.get("physics/2d/default_gravity")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print(_gravity)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	velocity.y += _gravity * delta
