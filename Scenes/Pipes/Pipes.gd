41f36eaac3a6b8ff824ca8b05ad254ae82f5468e
tolbek <ahmetfiratcs@gmail.com>
2025-05-18 20:00:57 +0300

    pipe exit console connected
    setup scrolling logic

---
+++ Scenes/Pipes/Pipes.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+const SPEED: float = 120.0
+const OFF_SCREEN: float = 100.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position.x -= SPEED * delta
+	
+	if position.x < get_viewport_rect().position.x - 100.0:
+		die()
+
+func die() -> void:
+	print("on_screen_exited")
+	set_process(false)
+	queue_free()
+
+
+func _on_screen_exited() -> void:
+	die()
