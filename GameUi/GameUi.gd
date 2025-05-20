861b19b627e02b68858c663dd6a6e1a74a058274
tolbek <ahmetfiratcs@gmail.com>
2025-05-20 17:37:49 +0300

    pre bug fix
    score update
    invisible wall

--- GameUi/GameUi.gd
+++ GameUi/GameUi.gd
@@ -1,10 +1,15 @@
 extends Control
 
+@onready var score_label: Label = $MarginContainer/ScoreLabel
 @onready var game_over_label: Label = $GameOverLabel
 @onready var press_space_label: Label = $PressSpaceLabel
 @onready var timer: Timer = $Timer
 
 var _can_press: bool = false
+var _score: int = 0
+
+func _ready() -> void:
+	_score = 0
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("Exit") and event.is_echo() == false:
@@ -14,7 +19,12 @@ func _unhandled_input(event: InputEvent) -> void:
 
 # Called when the node enters the scene tree for the first time.
 func _enter_tree() -> void:
+	SignalHub.on_point_scored.connect(on_point_scored)
 	SignalHub.on_plane_died.connect(on_plane_died)
+
+func on_point_scored() -> void:
+	_score += 1
+	score_label.text = "%04d" % _score
 
 func on_plane_died() -> void:
 	game_over_label.show()
--- Scenes/Pipes/Pipes.gd
+++ Scenes/Pipes/Pipes.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var laser: Area2D = $Laser
+
 const SPEED: float = 120.0
 const OFF_SCREEN: float = 100.0
 
@@ -28,3 +30,9 @@ func _on_screen_exited() -> void:
 func _on_pipe_body_entered(body: Node2D) -> void:
 	if body is Tappy:
 		body.die()
+
+
+func _on_laser_body_entered(body: Node2D) -> void:
+	if body is Tappy:
+		laser.body_entered.disconnect(_on_laser_body_entered)
+		SignalHub.emit_on_point_scored()
