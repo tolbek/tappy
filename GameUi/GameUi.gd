0c874dc28abb2152dbce8c7b7477c66599bc8e2c
tolbek <ahmetfiratcs@gmail.com>
2025-05-20 16:42:26 +0300

    game over mechanism functional

--- GameUi/GameUi.gd
+++ GameUi/GameUi.gd
@@ -1,14 +1,26 @@
 extends Control
 
+@onready var game_over_label: Label = $GameOverLabel
+@onready var press_space_label: Label = $PressSpaceLabel
+@onready var timer: Timer = $Timer
+
+var _can_press: bool = false
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("Exit") and event.is_echo() == false:
 		GameManager.load_game_scene()
+	elif _can_press == true and event.is_action_pressed("jump"):
+		GameManager.load_main_scene()
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _enter_tree() -> void:
+	SignalHub.on_plane_died.connect(on_plane_died)
 
+func on_plane_died() -> void:
+	game_over_label.show()
+	timer.start()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _on_timer_timeout() -> void:
+	_can_press = true
+	game_over_label.hide()
+	press_space_label.show()
